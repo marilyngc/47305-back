e501658acfcf3b4a94fa57acdf663704dc112af8
marilyn <marilyncelisgutierrez@gmail.com>
2023-09-21 23:57:03 -0300

    chat-websocket

--- chat-websockets/chat/src/app.js
+++ chat-websockets/chat/src/app.js
@@ -4,9 +4,10 @@ import {engine} from "express-handlebars";
 import { __dirname } from "./utils.js";
 import { viewsRouter } from "./routes/views.routes.js";
 import { Server } from "socket.io";
+import { Socket } from "socket.io";
 
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 
 //midlewares
@@ -29,3 +30,27 @@ app.set('views', path.join(__dirname,"/views")); // => /src/views
 
 // routes
 app.use(viewsRouter);
+
+
+let chat = [];
+
+// socket servidor
+io.on("connection",(socket)=>{
+    //cuando se conecta el usuario, le enviamos el historial del chat
+socket.emit("chatHistory", chat);
+
+// recibimos el mensaje de cada usuario
+    socket.on("msgChat", (data)=>{
+        console.log(data);
+        chat.push(data);
+
+        // enviamos el historial del chat a todos los ususrios conectados
+        io.emit("chatHistory",chat);
+    });
+
+    // recibimos mensaje de coneccion de nuevo cliente
+    socket.on("authenticated",(data)=>{
+        // => broadcast es para emitir la información al todos los usuarios menos al que se está logueando
+        socket.broadcast.emit("newUser",`El usuario ${data.name} se acaba de conectar `)
+    });
+});
--- chat-websockets/chat/src/public/js/home.js
+++ chat-websockets/chat/src/public/js/home.js
@@ -1,3 +1,64 @@
 console.log("JavaScrip en el frontend");
 
-const socket = io();
+const socketClient = io();
+const userName = document.getElementById("userName");
+const inputMsg = document.getElementById("inputMsg");
+const sendMsg = document.getElementById("sendMsg");
+const chatPanel = document.getElementById("chatPanel");
+
+
+let user; // variable de identidad del usuario
+
+swal.fire({ // => es una promesa
+    title: "Bienvenidos",
+    text: "Por favor, ingresa tu nombre de usuario",
+    input:"text",
+    // validamos los datos ingresados
+    inputValidator:(value) => {
+        return !value && "No ingresaste tu nombre tu usuario"
+    },
+    // para que al clickear fuera del span no se cierre
+    allowOutsideClick:false,
+    allowEscapeKey:false,
+}).then((inputValue)=>{
+   user = inputValue.value;
+   userName.innerHTML = user;
+   // mandamos un evento solo cuando el usuario se conecte
+   socketClient.emit("authenticated",user);
+});
+
+
+sendMsg.addEventListener("click", ()=>{
+    const msg = {user:user, message:inputMsg.value}
+    // mandamos el mensaje al servidor
+    socketClient.emit("msgChat",msg);
+});
+
+
+// recibe del servidor
+socketClient.on("chatHistory", (dataServer)=>{
+   let msgElements = "";
+   dataServer.forEach(element => {
+    // vamos agregandole parrafo a cada mensaje
+      msgElements+= `<p>Usuario: ${element.user}:  ${element.message}`
+   });
+
+   // mostramos el historial completo
+   chatPanel.innerHTML = msgElements;
+});
+
+
+// recibimos al cliente que se acaba de conectar
+socketClient.on("newUser",(data) =>{
+    // si el usuario ya se registró
+    if (user) {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: data,
+            showConfirmButton: false,
+            timer: 1500
+          });
+    }
+   
+});
